234bc208d3fc13da7d5d3bf5085f4bf3fc55011e
Somyajeet Singh <somyajeetsingh15@gmail.com>
2025-06-19 09:06:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import * type { ThemeProviderProps } from "next-themes/dist/types";
+import type { ThemeProviderProps } from "next-themes/dist/types";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
